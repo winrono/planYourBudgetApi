26e4c6eccc31f5b645a6d2f2c4305e2d63a0b9c4
MINSK\Raman_Nasovich <raman.nasovich@thomsonreuters.com>
2017-08-04 19:58:29 +0300

    minor refactoring

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new Foo();
+            f.Name = "first";
+            DoWork(f);
+
+            Console.WriteLine(f.Name);
+        }
+
+        static void DoWork(Foo f)
+        {
+            f.Name = "heeey!";
+        }
+    }
+
+    class Foo
+    {
+        public string Name { get; set; }
+    }
+}
